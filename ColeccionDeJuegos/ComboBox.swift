cb3173f0da67e7fe8b66346796bd760062b7ad07
dennys3mf <dennys.marquez@tecsup.edu.pe>
2023-10-29 16:09:27 -0500

    lab09

---
+++ ColeccionDeJuegos/ComboBox.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  ColeccionDeJuegos
+//
+//  Created by dennys3mf on 24/10/23.
+//
+
+import Foundation
+
+class ComboBox: UITextField, UITextFieldDelegate {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupComboBox()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupComboBox()
+    }
+    
+    private func setupComboBox() {
+        delegate = self
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Aquí puedes mostrar el UIPickerView o cualquier otra lógica que desees
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Otros métodos y lógica que puedas necesitar
+    
+}
